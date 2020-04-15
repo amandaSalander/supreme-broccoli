c09d3158d885170ab027307478cc79cbfa32221a
amandaSalander <ds_baraka@esi.dz>
2020-04-15 13:17:01 +0200

    added .gitignore file

--- node_js_tutorial/tutorials/async/002_async.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-
-fs.readFile('testfile.txt','utf8',function(err,data){
-    if(!err) {
-       console.log(data);
-    }
-});
-console.log("If you read this before the phone call,it is the proof ... better read it what comes next!");
